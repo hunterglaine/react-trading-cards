b085ad6391edb0cf7de76ca93d35ae6c1247e18d
Hunter Laine <hunterglaine@gmail.com>
2021-02-05 12:18:11 -0800

    Build out homepage content

--- static/js/homepage.jsx
+++ static/js/homepage.jsx
@@ -3,7 +3,9 @@
 function Homepage() {
   return (
     <div>
-      This component needs to be implemented.
+      <h1>Welcome User</h1>
+      <a href="/cards">Check Out the Cards Here</a><br/>
+      <img src="/static/img/balloonicorn.jpg" alt="Image of Balloonicorn"/>
     </div>
   );
 }
